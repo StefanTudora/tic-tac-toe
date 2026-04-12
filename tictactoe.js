3fe32bdcd1968028bc86ca1801ec30d9958f77f9
Stefan Tudora <24649875+StefanTudora@users.noreply.github.com>
2026-04-12 12:48:03 +0300

    Added cpu functionality

--- tictactoe.js
+++ tictactoe.js
@@ -6,14 +6,15 @@ const board = Array(3).fill().map(() => Array(3).fill('#'));
 
 // Player factory function
 const createPlayer = function (boardSign) {
-    this.boardSign = boardSign;
-
-    const getSing = function () {
-        return this.boardSign;
+    return {
+        boardSign,
+        getSign() {
+            return this.boardSign;
+        }
     }
 }
 
-const playerMap = { "CPU": createPlayer('O'), "Human": createPlayer('X') };
+const playerMap = { "CPU": createPlayer("O"), "Human": createPlayer("X") };
 
 class GameBoard {
     constructor(board) {
@@ -36,13 +37,26 @@ class GameBoard {
             this.board[row][col] = mark;
         }
 
+        this.isFull = function () {
+            return this.freeCells === 0;
+        }
+
         this.getSpot = function (row, col) {
             return this.board[row][col];
+        }
+
+        this.clearSpot = function (row, col) {
+            this.board[row][col] = "#";
+            ++this.freeCells;
         }
 
         this.clearBoard = function () {
             this.board = Array(3).fill().map(() => Array(3).fill("#"));
             this.freeCells = 9;
+        }
+
+        this.isSpotFree = function (row, col) {
+            return board[row][col] === "#";
         }
 
         function allEqual(input) {
@@ -53,7 +67,7 @@ class GameBoard {
             // Check rows
             for (let i = 0; i < 3; ++i) {
                 const rowSum = this.board[i].reduce((accumulator, currentValue) => accumulator + currentValue, "");
-                if (allEqual(rowSum)) {
+                if (allEqual(rowSum) && rowSum[0] !== '#') {
                     return rowSum[0];
                 }
             }
@@ -63,7 +77,7 @@ class GameBoard {
                 for (let j = 0; j < 3; ++j) {
                     columnSum += this.board[j][i];
                 }
-                if (allEqual(columnSum)) {
+                if (allEqual(columnSum) && columnSum[0] !== '#') {
                     return columnSum[0];
                 }
             }
@@ -72,15 +86,15 @@ class GameBoard {
             for (let i = 0; i < 3; ++i) {
                 diagSum += this.board[i][i];
             }
-            if (allEqual(diagSum)) {
+            if (allEqual(diagSum) && diagSum[0] !== '#') {
                 return diagSum[0];
             }
             // Check secondary diagonal
             diagSum = "";
             for (let i = 0; i < 3; ++i) {
-                diagSum += this.board[i][i];
+                diagSum += this.board[i][2 - i];
             }
-            if (allEqual(diagSum)) {
+            if (allEqual(diagSum) && diagSum[0] !== '#') {
                 return diagSum[0];
             }
             return null;
@@ -99,6 +113,7 @@ class GameBoard {
 
 class GameManager {
     constructor(board) {
+
         this.board = board;
         this.state = undefined;
         this.counter = 0;
@@ -113,60 +128,75 @@ class GameManager {
             this.state = state;
         };
 
-        this.play = function () {
-            while (!(this.state instanceof FinalState)) {
-                console.log("Keep playing");
-                this.state.doAction();
-            }
-        };
-
         // Remove in final implementation
         this.incrementAndGet = function () {
             ++this.counter;
             return this.counter;
         };
+
+        this.doAction = function () {
+            this.state.doAction();
+        }
     }
 }
 
 class HumanState {
     constructor(game) {
+
         this.game = game;
-        this.playerInfo = createPlayer('X');;
+        this.playerInfo = createPlayer('X');
+
         this.doAction = function () {
-            console.log("We are doing an action on the Human state");
-            if (game.incrementAndGet() > 10) {
-                game.setState(new FinalState(game))
-            } else {
-                game.setState(new CheckState(game, this));
+            // Await player action
+        }
+
+        this.resumeAction = function (cell, idx) {
+            let row = Math.floor(idx / 3);
+            let col = idx % 3;
+            if (!game.board.isSpotFree(row, col)) {
+                // Wait for valid player selection
+                return;
             }
+            cell.appendChild(getXSvg());
+            game.board.setSpot(row, col, "X");
+            game.setState(new CheckState(game, this));
+            game.doAction();
         }
     }
 }
 
 class CPUState {
     constructor(game) {
+
         this.game = game;
-        this.playerInfo = createPlayer('O');
+        this.playerInfo = createPlayer("O");
+        this.miniMaxAlg = new MiniMax(game.board);
+
         this.doAction = function () {
+            // Calculate optimal move for the CPU
+            const optiamlMove = this.miniMaxAlg.getBestChoice();
+            const row = optiamlMove[0];
+            const col = optiamlMove[1];
+            game.board.setSpot(row, col, "O");
+            document.querySelectorAll(".board > div")[row * 3 + col].appendChild(getOSvg());
             console.log("We are doing an action on the CPU state");
-            if (game.incrementAndGet() > 10) {
-                game.setState(new FinalState(game))
-            } else {
-                game.setState(new CheckState(game, this));
-            }
+            game.setState(new CheckState(game, this));
+            game.doAction();
         }
     }
 }
 
 class CheckState {
     constructor(game, prevState) {
+
         this.game = game;
         this.prevState = prevState;
+
         this.doAction = function () {
             const winningPlayer = game.board.isThereAWinner();
             if (winningPlayer !== null) {
                 console.log("We have a winner");
-            } else if (board.getFreeSpots() == 0) {
+            } else if (game.board.getFreeSpots() == 0) {
                 game.setState(new FinalState(game));
             }
             // use prevState to know whos turn it is
@@ -175,6 +205,7 @@ class CheckState {
             } else {
                 game.setState(new HumanState(game));
             }
+            game.doAction();
         }
     }
 }
@@ -183,6 +214,7 @@ class FinalState {
     // Reset the game here
     constructor(game) {
         game.board.clearBoard();
+        game.setState(new HumanState(game));
     }
 }
 
@@ -191,15 +223,103 @@ var game = new GameManager(new GameBoard(board));
 
 game.setState(new HumanState(game));
 
-game.play();
+// game.play();
 
 /*
     Minimax implementation
 */
 
 class MiniMax {
-    constructor() {
-        this.miniPlayer = createPlayer('O');
-        this.maxPlayer = createPlayer('X');
+    constructor(board) {
+
+        this.board = board;
+
+        this.miniMaxEval = function (player) {
+            // Check for a winner in this round
+            const winner = board.isThereAWinner();
+            if (winner !== null) {
+                return winner === "X" ? 1 : -1;
+            }
+            // No more moves left, draw
+            if (board.isFull()) {
+                return 0;
+            }
+            if (player.getSign() === "X") {
+                var maxValue = -1e6;
+                for (let row = 0; row < 3; ++row) {
+                    for (let col = 0; col < 3; ++col) {
+                        if (board.getSpot(row, col) === "#") {
+                            board.setSpot(row, col, "X");
+                            maxValue = Math.max(maxValue, this.miniMaxEval(createPlayer("O")));
+                            board.clearSpot(row, col);
+                        }
+                    }
+                }
+                return maxValue;
+            } else if (player.getSign() === "O") {
+                var minValue = +1e6;
+                for (let row = 0; row < 3; ++row) {
+                    for (let col = 0; col < 3; ++col) {
+                        if (board.getSpot(row, col) === "#") {
+                            board.setSpot(row, col, "X");
+                            minValue = Math.min(minValue, this.miniMaxEval(createPlayer("X")));
+                            board.clearSpot(row, col);
+                        }
+                    }
+                }
+                return minValue;
+            } else {
+                console.log("Unkwown signature");
+                return 0;
+            }
+        }
+
+        // Method used by the CPU
+        this.getBestChoice = function () {
+            var bestMove = [-1, -1];
+            var minValue = 1e6;
+            for (let row = 0; row < 3; ++row) {
+                for (let col = 0; col < 3; ++col) {
+                    if (board.getSpot(row, col) === "#") {
+                        board.setSpot(row, col, "0");
+                        let currentMinValue = Math.min(minValue, this.miniMaxEval(createPlayer("X")));
+                        board.setSpot(row, col, "#");
+                        if (currentMinValue < minValue) {
+                            minValue = currentMinValue;
+                            bestMove = [row, col];
+                        }
+                    }
+                }
+            }
+            return bestMove;
+        }
     }
 }
+
+function attachListener() {
+    const cellsList = document.querySelectorAll(".board > div");
+    cellsList.forEach((cell, idx) => {
+        cell.addEventListener("click", () => {
+            if (game.getState() instanceof HumanState) {
+                game.state.resumeAction(cell, idx);
+            } else {
+                // Log error;
+                console.log("Action triggered by someone not human");
+            }
+        });
+    });
+}
+
+function getXSvg() {
+    const xImg = document.createElement("img");
+    xImg.setAttribute("src", "assets/x-symbol.svg");
+    return xImg;
+}
+
+function getOSvg() {
+    const xImg = document.createElement("img");
+    xImg.setAttribute("src", "assets/o-symbol.svg");
+    return xImg;
+}
+
+attachListener();
